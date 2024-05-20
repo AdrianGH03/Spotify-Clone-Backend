207a15bed0c7f54e982761118bf7c38be91cdde3
Miguel Gomez <miguelgomezca2021@gmail.com>
2024-05-20 13:18:40 -0400

    Changing cors test 4

--- server.js
+++ server.js
@@ -61,11 +61,7 @@ data.append('client_id', clientId);
 data.append('client_secret', clientSecret);
 
 //Middleware
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+
 
 app.use(express.static(__dirname));
 
